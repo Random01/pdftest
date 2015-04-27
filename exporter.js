86dd2ff64bc65612603786adf65cd1fcb222329f
Alexsei Matvienko <alexsei_matvienko@epam.com>
2015-04-27 14:20:05 +0300

    add server class

---
+++ exporter.js
@@ -0,0 +1,19 @@
+﻿var fs = require('fs');
+var PDFDocument = require('pdfkit');
+
+var doc = new PDFDocument();
+
+doc.pipe(fs.createWriteStream('d:/tmp/output.pdf'));
+
+doc.fontSize(10)
+    .text('Service unavailable. Please resubmit your request.', 100, 100);
+
+doc.fontSize(10)
+    .font('fonts/ipaexg.ttf')
+    .text('サービスが利用できません。リクエストを再度実行してください。', 100, 120);
+
+doc.fontSize(10)
+    .font('fonts/simfang.ttf')
+    .text('服务不可用，请重新提交您的请求。', 100, 140);
+
+doc.end();
--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-﻿var fs = require('fs');
-var PDFDocument = require('pdfkit');
+﻿var server = require('./server');
+var router = require('./router');
 
-var doc = new PDFDocument();
-
-doc.pipe(fs.createWriteStream('d:/tmp/output.pdf'));
-
-doc.fontSize(10)
-    .text('Service unavailable. Please resubmit your request.', 100, 100);
-
-doc.fontSize(10)
-    .font('fonts/ipaexg.ttf')
-    .text('サービスが利用できません。リクエストを再度実行してください。', 100, 120);
-
-doc.fontSize(10)
-    .font('fonts/simfang.ttf')
-    .text('服务不可用，请重新提交您的请求。', 100, 140);
-
-doc.end();
+server.start(router.route);
