881bea2ff9acb783318d0294887d0dff0be4320d
Alexsei Matvienko <alexsei_matvienko@epam.com>
2015-04-27 18:03:18 +0300

    update project type

---
+++ pdfExporter.js
@@ -0,0 +1,24 @@
+﻿var fs = require('fs');
+var PDFDocument = require('pdfkit');
+
+function exportToPdf(stream) {
+
+    var doc = new PDFDocument();
+
+    doc.pipe(stream);
+
+    doc.fontSize(10)
+        .text('Service unavailable. Please resubmit your request.', 100, 100);
+
+    doc.fontSize(10)
+        .font('fonts/ipaexg.ttf')
+        .text('サービスが利用できません。リクエストを再度実行してください。', 100, 120);
+
+    doc.fontSize(10)
+        .font('fonts/simfang.ttf')
+        .text('服务不可用，请重新提交您的请求。', 100, 140);
+
+    doc.end();
+}
+
+exports.exportToPdf = exportToPdf;
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-﻿var http = require('http');
-var url = require('url');
+﻿var http = require('http'),
+    url = require('url'),
+    formidable = require('formidable'),
+    util = require('util');
 
-function start(route) {
+
+function start(exportToPdf) {
     function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log('Request for ' + pathname + ' received.');
 
-        route(pathname);
+        if (request.url == '/pdf' && request.method.toLowerCase() == 'post') {
+            var form = new formidable.IncomingForm();
+            form.parse(request, function (err, fields) {
+                var fileName = fields.fileName;
+                var documentDefinition = JSON.parse(fields.exportData);
 
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Hello World");
+                response.writeHead(200, {
+                    'Content-Type': 'application/pdf',
+                    'Content-Disposition': 'attachment; filename=test.pdf'
+                });
+                exportToPdf(response);
+            });
+
+            return;
+        }
+
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello!');
         response.end();
     }
 
