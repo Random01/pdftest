38fafac26bf81186f1def659709e2660c80e39e3
Alexsei Matvienko <alexsei_matvienko@epam.com>
2015-04-29 10:16:59 +0300

    add fonts

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 ﻿var http = require('http'),
     url = require('url'),
     formidable = require('formidable'),
-    util = require('util');
-
+    util = require('util'),
+    pdfMakePrinter = require('pdfmake'),
+    fs = require('fs');
 
 function start(exportToPdf) {
     function onRequest(request, response) {
+        
+        console.log('Request has received');
 
         if (request.url == '/pdf' && request.method.toLowerCase() == 'post') {
             var form = new formidable.IncomingForm();
             form.parse(request, function (err, fields) {
+                
                 var fileName = fields.fileName;
                 var documentDefinition = JSON.parse(fields.exportData);
+                var fontDescriptors = {
+                    Roboto: {
+                        normal: 'fonts/ProximaNovaRegular.ttf',
+                        bold: 'fonts/ProximaNovaSemibold.ttf',
+                    }
+                };
+                var printer = new pdfMakePrinter(fontDescriptors);
+                var doc = printer.createPdfKitDocument(documentDefinition);
+                doc.pipe(fs.createWriteStream('d:/tmp/' + fileName + '.pdf'));
+                doc.end();
 
-                response.writeHead(200, {
-                    'Content-Type': 'application/pdf',
-                    'Content-Disposition': 'attachment; filename=test.pdf'
-                });
-                exportToPdf(response);
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
+                response.write('Success');
+                response.end();
             });
 
             return;
